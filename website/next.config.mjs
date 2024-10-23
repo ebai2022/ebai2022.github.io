62a3af465703b21b3fe416f3b54a2491e2a03463
Ethan Bai <ebai2022@cs.washington.edu>
2024-10-23 21:43:48 +0200

    Fixing Next.js for Github Pages

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export', // Enables static export
+    basePath: process.env.NODE_ENV === 'production' ? '/ebai2022.github.io' : '', // Base path for GitHub Pages
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/ebai2022.github.io/' : '', // Prefix for assets in GitHub Pages
+    images: {
+        unoptimized: true, // Disable Image Optimization since Next.js image optimization doesn't work with static export
+    },
+};
 
 export default nextConfig;
