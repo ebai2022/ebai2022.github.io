67007cc154a3aa576857baf486a3f20ab9ec2360
Ethan Bai <ebai2022@cs.washington.edu>
2024-10-23 23:01:21 +0200

    update site

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -1,4 +1,11 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'export',  // Creates a static export
+    images: {
+      unoptimized: true, // Required for static export
+    },
+    // Remove basePath since this is a user site
+}
+  
+export default nextConfig
